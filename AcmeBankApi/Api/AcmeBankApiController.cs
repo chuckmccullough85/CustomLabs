228d970b2ee755af0087c97411d38078f3b6ad2f
Chuck McCullough <chuck@mcculloughassociates.com>
2023-06-22 14:31:05 -0500

    Added new Web api project

---
+++ AcmeBankApi/Api/AcmeBankApiController.cs
@@ -0,0 +1,25 @@
+﻿using AcmeLib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcmeBankApi.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcmeBankApiController : ControllerBase
+    {
+        [HttpGet, Route("getaccounts")]
+        public async Task<IActionResult> GetAllAccounts(string email, IBankService svc)
+        {
+            try
+            {
+                return Ok(await svc.GetAccounts(email));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+          
+        }
+    }
+}
